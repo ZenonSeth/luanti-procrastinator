f1a36ea7208be5a17cef126d69796631e4b5ab35
Zenon Seth <Zenon.Seth@gmail.com>
2026-08-25 13:17:44 +0100

    Add more excuses

--- procrastinate.lua
+++ procrastinate.lua
@@ -10,6 +10,12 @@ local place_excuses = {
   "I'm too tired.",
   "Yeah, no. No thanks.",
   "Don't rush me, I'm thinking!",
+  "Ask me again after lunch.",
+  "I'm on a break.",
+  "That spot looks tired, let it rest.",
+  "Ugh, fine.. actually no.",
+  "I need to stretch first.",
+  "I'll add it to my to-do list.",
 }
 
 local dig_excuses = {
@@ -21,6 +27,12 @@ local dig_excuses = {
   "I'll get to it eventually.",
   "Ehh. Some other day.",
   "Don't rush me, I'm thinking!",
+  "Digging is a big commitment.",
+  "That node has rights too.",
+  "One does not simply dig on command.",
+  "Ask again later, I'm busy with.. something.",
+  "I need to think about this one.",
+  "Nah, I'm good.",
 }
 
 local function wrap_on_place(original_on_place)
